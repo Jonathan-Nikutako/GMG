a8048b3ed1e5495524aeaf341d90e3d4a7d04b98
atsushi <atsushi.pc.po_oq@outlook.jp>
2020-12-10 18:25:11 +0900

    プレイヤー(移動、ダッシュ、スタミナ)実装

---
+++ jo_gameproject_2020_09/Assets/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float x_Axis { private set; get; }
+    public float y_Axis { private set; get; }
+
+    public bool DashKey { private set; get; }
+
+    public bool JumpKey { private set; get; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DashKey = Input.GetAxis("Dash") > 0;
+        JumpKey = Input.GetAxisRaw("Jump") > 0;
+    }
+    private void FixedUpdate()
+    {
+        x_Axis = Input.GetAxis("Horizontal");
+        y_Axis = Input.GetAxis("Vertical");
+    }
+}
